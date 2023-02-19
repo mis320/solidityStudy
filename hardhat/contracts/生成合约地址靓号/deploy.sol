b2af85e392df5032ee05a01180e40afa2290b995
mis16 <2422914657@qq.com>
2023-02-19 22:38:55 +0800

    test

--- hardhat/contracts/生成合约地址靓号/deploy.sol
+++ hardhat/contracts/生成合约地址靓号/deploy.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.6.12;
-//0x8ec195ee65b95741490f9b682667ce503c0ca4d9
+//0x435F37f373c47Ac9B5257E7dD8692F859cea3A91
 //0x652bcabc0aff25eac0c563a4a017f7b518fc67c8359f34a5c25920602e16e07c
 interface ChiToken {
     function freeFromUpTo(address from, uint256 value) external;
@@ -9,8 +9,23 @@ interface ChiToken {
     function deploy(bytes memory bytecode,uint salt) external view returns (address contractAddress);
 }
 contract deployer {
-    
+    address public immutable  owner;
     ChiToken constant  chi = ChiToken(0x0000000000004946c0e9F43F4Dee607b0eF1fA1c);
+
+
+
+    constructor() public{
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(
+            owner == msg.sender ,
+            "Ownable: caller is not the owner"
+        );
+        _;
+    }
+
     modifier discountCHI {
         uint256 gasStart = gasleft();
         _;
@@ -18,7 +33,7 @@ contract deployer {
         chi.freeFromUpTo(msg.sender, (gasSpent + 14154) / 41947);
     }
  
-    function deploy(bytes memory bytecode,bytes32 salt0) external returns (address contractAddress){
+    function deploy(bytes memory bytecode,bytes32 salt0) external onlyOwner returns (address contractAddress){
         bytes32 salt = keccak256(abi.encodePacked(salt0));
         assembly {
             contractAddress := create2(0, add(bytecode, 32), mload(bytecode), salt)
@@ -26,7 +41,8 @@ contract deployer {
         require(contractAddress!=address(0),"a0");
     }
     
-    function deploy2(bytes memory bytecode,bytes32 salt) external discountCHI returns (address contractAddress) {
+    function deploy2(bytes memory bytecode,bytes32 salt0) external onlyOwner discountCHI returns (address contractAddress) {
+        bytes32 salt = keccak256(abi.encodePacked(salt0));
         assembly {
             contractAddress := create2(0, add(bytecode, 32), mload(bytecode), salt)
         }
@@ -36,14 +52,7 @@ contract deployer {
     function initCodeHash(bytes memory bytecode) external pure returns (bytes32 contractAddress){
         contractAddress  =_initCodeHash(bytecode);
     }
-    function toBytes32(uint salt) external pure returns (bytes32 b){
-       b =  bytes32(salt);
-    }
-
-
-     function k256(uint salt) external pure returns (bytes32 b){
-       b = keccak256(abi.encodePacked(salt));
-    }
+   
     function _initCodeHash(bytes memory bytecode) private pure returns (bytes32 contractAddress){
         contractAddress  =keccak256(abi.encodePacked(bytecode));
     }
@@ -56,6 +65,4 @@ contract deployer {
                 _initCodeHash(bytecode)
             ))));
     }
-
-
 }
